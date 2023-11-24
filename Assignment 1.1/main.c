4120d74e3628fbc16c518485947eac700a719a20
ShreyasDambe <101385887+ShreyasDambe@users.noreply.github.com>
2023-11-24 19:50:12 +0530

    Take Single Integer Thriugh A to Z

---
+++ Assignment 1.1/main.c
@@ -0,0 +1,28 @@
+
+              //////////////////////////////////////////////
+             //                                          //
+            //   Take Single Integer Thriugh A to Z     //
+           //                                          //
+          //////////////////////////////////////////////
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    int ivalue = 0;
+
+     for(ivalue = 65;ivalue <= 90;ivalue++)
+     {
+         printf("%c\n",ivalue);
+     }
+
+     getch();
+
+     return 0;
+}
+
+
