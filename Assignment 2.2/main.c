3c88a744698f1b013a129912139df18e0322a0f3
ShreyasDambe <101385887+ShreyasDambe@users.noreply.github.com>
2023-11-24 20:02:55 +0530

    Add files via upload

---
+++ Assignment 2.2/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void Display()
+{
+    int i=0;
+    for(i=1;i<=5;i++)
+    {
+        printf("Linus\n");
+    }
+
+}
+
+int main()
+{
+    Display();
+
+    return 0;
+}
